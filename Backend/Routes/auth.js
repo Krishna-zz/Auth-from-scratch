7c4b0cac381acf41e4d727affcf487cc8b112ebf
Krishna-zz <krishnagunjal022@gmail.com>
2025-10-31 08:11:48 +0530

    Made routes and auth setup init

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -2,17 +2,49 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const user = require('../Models/user.model')
+const User = require('../Models/user.model')
 
 
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
     
+  try {
+    const {name, email , password} = req.body
+
+    const user = await User.findOne({email})
+    if(user) return res.status(400).json({message:"User already exists"})
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({name, email, password:hashedPassword})
+    await newUser.save()
+
+    res.json({message:"User registered successfully"})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message:"Internal server error"})
+  }
 })
 
 
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
 
+   try {
+     const {email , password} = req.body
+
+    const user = await User.findOne({email})
+    if(!user) return res.status(400).json({message:"Invalid Credentials"})
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) return res.status(400).json({message:"Invalid Credentials"})
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET  , {expiresIn:"1h"})
+
+    const {password:kd, ...userData} = user._doc
+
+    res.json({message:"Login Successful", token, user: userData})
+   } catch (error) {
+    res.status(500).json({message:"Internal Server Error"})
+   }
 })
 
 
