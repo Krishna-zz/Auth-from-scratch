c9a15ecedd5a73b0fe806e37abad35adf56dfbe4
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-01 09:29:50 +0530

    Login page completed

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -34,7 +34,7 @@ router.post('/login', async(req, res) => {
     const user = await User.findOne({email})
     if(!user) return res.status(400).json({message:"Invalid Credentials"})
 
-    const isMatch = await bcrypt.compare(password, user.password)
+    const isMatch = bcrypt.compare(password, user.password)
     if(!isMatch) return res.status(400).json({message:"Invalid Credentials"})
 
     const token = jwt.sign({id: user._id}, process.env.JWT_SECRET  , {expiresIn:"1h"})
